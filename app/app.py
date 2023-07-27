d2083f94f131ebaf0e88351d0cbd5ff41f63572f
Emerson Espinoza <xinayespinoza@gmail.com>
2023-07-27 19:50:23 -0400

    personalizar puerto

--- app/app.py
+++ app/app.py
@@ -8,10 +8,14 @@ app = Flask(__name__)
 # segunda forma de poner rutas
 
 def index2():
-    return "hola de segunda forma"
+    return "hola de segunda forma estamos mejor"
 
+# creando una ruta extra
+@app.route('/holamundo')
+def hola_mundo():
+    return "Hola mundo de FLask"
 # bloque de pruebas
 if __name__ == "__main__":
     app.add_url_rule('/', view_func=index2)
-    
-    app.run()
+    # personalizando puertos y no reiniciar a cada rato el servidor
+    app.run(debug=True, port=5005)
