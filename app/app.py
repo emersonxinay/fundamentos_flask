49b93f2de37f5edcd2abb684887082dd48d50653
Emerson Espinoza <xinayespinoza@gmail.com>
2023-07-27 19:44:40 -0400

    segunda forma de correr flask con rutas

--- app/app.py
+++ app/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 # rutas
-@app.route('/')
-def index():
-    return "Hola amigos"
+# @app.route('/')
+# def index():
+#     return "Hola amigos"
+
+# segunda forma de poner rutas
+
+def index2():
+    return "hola de segunda forma"
 
 # bloque de pruebas
 if __name__ == "__main__":
+    app.add_url_rule('/', view_func=index2)
     
     app.run()
